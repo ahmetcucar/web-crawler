629d06e3f6c38c20b764b4e198d5d30c7313980c
ahmetcucar <acucar@uw.edu>
2023-11-27 15:20:02 -0800

    getURLSFromHTML function + tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,13 @@
+const { JSDOM } = require('jsdom')
+
+// normalizeURL
+// It takes a URL as input and returns a normalized URL.
+// Normalized URLs have the following properties:
+// - The protocol is removed (http:// or https://)
+// - The domain is lowercased
+// - The path is preserved
+// - The trailing slash is removed
+// - The URL is returned as a string
 function normalizeURL(url) {
     const urlObj = new URL(url)
     const hostPath = urlObj.host + urlObj.pathname
@@ -8,7 +18,25 @@ function normalizeURL(url) {
 
 }
 
+// getURLSFromHTML
+// It takes a string of HTML as input and returns a list of all the link URLs.
+// We'll use the JSDOM library to parse the HTML and extract the links.
+// baseURL = the root URL of the website we're crawling.
+// This will allow us to rewrite relative URLs into absolute URLs.
+function getURLSFromHTML(htmlBody, baseURL) {
+    urls = []
+    const dom = new JSDOM(htmlBody)
+    const links = dom.window.document.querySelectorAll('a')
+    links.forEach(link => {
+        const href = link.getAttribute('href')
+        if (href.startsWith('http')) {
+            urls.push(href)
+        } else {
+            urls.push(baseURL + href)
+        }
+    })
+    return urls
 
-module.exports = {
-    normalizeURL
 }
+
+module.exports = { normalizeURL, getURLSFromHTML }
--- crawl.test.js
+++ crawl.test.js
@@ -1,12 +1,65 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLSFromHTML } = require('./crawl.js')
 
-test('normalizeURL', () => {
-    expect(normalizeURL('https://www.example.com')).toBe('www.example.com')
-    expect(normalizeURL('https://www.Example.com')).toBe('www.example.com')
-    expect(normalizeURL('https://www.example.com/')).toBe('www.example.com')
-    expect(normalizeURL('https://www.EXAmple.com/')).toBe('www.example.com')
-    expect(normalizeURL('https://www.example.com/foo')).toBe('www.example.com/foo')
-    expect(normalizeURL('https://www.example.com/foo/')).toBe('www.example.com/foo')
-    expect(normalizeURL('https://www.example.com/foo/bar')).toBe('www.example.com/foo/bar')
-})
+describe('normalizeURL Function', () => {
+    test('removes https protocol', () => {
+      const input = 'https://www.example.com'
+      const expected = 'www.example.com'
+      expect(normalizeURL(input)).toEqual(expected)
+    })
+
+    test('removes http protocol', () => {
+      const input = 'http://www.example.com'
+      const expected = 'www.example.com'
+      expect(normalizeURL(input)).toEqual(expected)
+    })
+
+    test('handles mixed case domain', () => {
+      const input = 'https://www.ExaMple.COM'
+      const expected = 'www.example.com'
+      expect(normalizeURL(input)).toEqual(expected)
+    })
+
+    test('retains path in the URL', () => {
+      const input = 'https://www.example.com/path/to/resource'
+      const expected = 'www.example.com/path/to/resource'
+      expect(normalizeURL(input)).toEqual(expected)
+    })
+
+    test('removes trailing slash', () => {
+      const input = 'https://www.example.com/'
+      const expected = 'www.example.com'
+      expect(normalizeURL(input)).toEqual(expected)
+    })
+  })
+
+describe('getURLsFromHTML Function', () => {
+    test('extracts URLs from simple HTML string', () => {
+      const htmlBody = '<a href="https://www.example.com">Example</a>';
+      const baseURL = 'https://www.test.com';
+      const expected = ['https://www.example.com'];
+      expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(expected);
+    });
+
+    test('handles relative URLs by appending baseURL', () => {
+      const htmlBody = '<a href="/path/to/resource">Resource</a>';
+      const baseURL = 'https://www.test.com';
+      const expected = ['https://www.test.com/path/to/resource'];
+      expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(expected);
+    });
+
+    test('returns an empty array for HTML with no links', () => {
+      const htmlBody = '<p>No links here</p>';
+      const baseURL = 'https://www.test.com';
+      const expected = [];
+      expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(expected);
+    });
+
+    test('extracts multiple URLs from HTML', () => {
+      const htmlBody = '<a href="https://www.example.com">Example</a><a href="https://www.test.com/page">Test</a>';
+      const baseURL = 'https://www.test.com';
+      const expected = ['https://www.example.com', 'https://www.test.com/page'];
+      expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(expected);
+    });
+
+  });
